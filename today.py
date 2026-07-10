901f6543b67267d875bee4a42891d22e128a243a
rajatprajapati961-cmyk <rajatprajapati961@gmail.com>
2026-07-10 09:58:13 +0530

    Remove login functionality from today.py

--- today.py
+++ today.py
@@ -1,11 +1,1 @@
-username = ("rajat 123")
-password = ("5656")
 
-username_input = input("Enter your username: ")
-password_input = input("Enter your password: ")
-
-if username_input == username and password_input == password:
-    print("Login successful!")
-
-else:
-    print("Invalid username or password. Please try again.")
